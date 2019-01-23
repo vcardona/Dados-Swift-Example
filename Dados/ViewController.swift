7b4cc8bf43cf1fb11cde116b23f5089db00bcb08
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-01-23 13:05:53 -0500

    Add new UI Conections

--- Dados/ViewController.swift
+++ Dados/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton)
+    {
+        
+    }
+    
 }
-
